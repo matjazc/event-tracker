3236cd86a54818f188bb8a60470d8f29362f02f3
Matjaž Cvek <matjaz.cvek@gmail.com>
2025-02-13 22:41:38 +0100

    add event id option

--- services/src/events/events.service.ts
+++ services/src/events/events.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 import { RolesService } from 'src/roles/roles.service';
@@ -24,9 +29,32 @@ export class EventsService {
   async create(createEventDto: Prisma.EventCreateInput, request: Request) {
     await this.checkRolePermission(request, createEventDto.type);
 
-    return this.databaseService.event.create({
-      data: createEventDto,
-    });
+    if (typeof createEventDto.eventId !== 'number') {
+      throw new BadRequestException('Id must be an integer.');
+    }
+
+    try {
+      return await this.databaseService.event.create({
+        data: createEventDto,
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new HttpException(
+          `Event with ID ${createEventDto.eventId} already exists.`,
+          HttpStatus.CONFLICT,
+        );
+      } else if (error instanceof Prisma.PrismaClientValidationError) {
+        throw new BadRequestException('Invalid data provided.');
+      }
+
+      throw new HttpException(
+        'An unexpected error occurred while creating the event.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async findAll() {
@@ -45,12 +73,35 @@ export class EventsService {
     const updateEventType = updateEventDto.type as string;
     await this.checkRolePermission(request, updateEventType);
 
-    return this.databaseService.event.update({
-      where: {
-        id,
-      },
-      data: updateEventDto,
-    });
+    if (typeof updateEventDto.eventId !== 'number') {
+      throw new BadRequestException('Id must be an integer.');
+    }
+
+    try {
+      return await this.databaseService.event.update({
+        where: {
+          id,
+        },
+        data: updateEventDto,
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new HttpException(
+          `Event with ID ${updateEventDto.eventId} already exists.`,
+          HttpStatus.CONFLICT,
+        );
+      } else if (error instanceof Prisma.PrismaClientValidationError) {
+        throw new BadRequestException('Invalid data provided.');
+      }
+
+      throw new HttpException(
+        'An unexpected error occurred while creating the event.',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async remove(id: number) {
