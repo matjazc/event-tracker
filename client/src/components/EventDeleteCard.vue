be5d7a9c45b2c5204d3835168a2d9799b2fafd67
Matjaž Cvek <matjaz.cvek@gmail.com>
2025-02-14 14:42:50 +0100

    refactor table component

---
+++ client/src/components/EventDeleteCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineEmits(['cancel', 'confirm'])
+</script>
+
+<template>
+  <v-card>
+    <v-card-title class="text-h5">Are you sure you want to delete this event?</v-card-title>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="blue-darken-1" variant="text" @click="$emit('cancel')">Cancel</v-btn>
+      <v-btn color="blue-darken-1" variant="text" @click="$emit('confirm')">OK</v-btn>
+      <v-spacer></v-spacer>
+    </v-card-actions>
+  </v-card>
+</template>
